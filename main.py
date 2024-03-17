57710a49103975bb2be5140ea1ea53665cde4e85
tarush10000 <tarushagarwal2003@gmail.com>
2024-03-17 22:40:05 +0530

    started UI

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+import sys, os
+import lag
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
